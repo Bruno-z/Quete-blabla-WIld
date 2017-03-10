f51da4611595ad1711cf7ee16e07ad7a7439eacf
Zilio Bruno <bruno.zilio31@gmail.com>
2017-03-10 16:53:28 +0100

    ajout du datepicker

--- app/src/main/java/wildcodeschool/blablawild/SearchItineraryActivity.java
+++ app/src/main/java/wildcodeschool/blablawild/SearchItineraryActivity.java
@@ -1,13 +1,18 @@
 package wildcodeschool.blablawild;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
+import android.icu.util.Calendar;
 import android.net.Uri;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
@@ -17,6 +22,9 @@ import com.google.android.gms.appindexing.AppIndex;
 import com.google.android.gms.appindexing.Thing;
 import com.google.android.gms.common.api.GoogleApiClient;
 
+import java.text.SimpleDateFormat;
+import java.util.Locale;
+
 
 public class SearchItineraryActivity extends AppCompatActivity {
 
@@ -25,6 +33,8 @@ public class SearchItineraryActivity extends AppCompatActivity {
     EditText editText10;
     Button button2;
     public final static String EXTRA_OBJET = "donne";
+    Calendar myCalendar;
+
 
 
     /**
@@ -33,6 +43,7 @@ public class SearchItineraryActivity extends AppCompatActivity {
      */
     private GoogleApiClient client;
 
+    @RequiresApi(api = Build.VERSION_CODES.N)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -43,10 +54,12 @@ public class SearchItineraryActivity extends AppCompatActivity {
         editText10 = (EditText) findViewById(R.id.editText10);
         button2 = (Button) findViewById(R.id.button2);
 
+
         Intent intent = getIntent();
 
 
         button2.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.N)
             @Override
             public void onClick(View v) {
 
@@ -55,10 +68,10 @@ public class SearchItineraryActivity extends AppCompatActivity {
                     String Destination = editText9.getText().toString();
                     String Date = editText10.getText().toString();
 
-                    SearchRequestModel objet =new SearchRequestModel(Depart, Destination, Date);
-                    Intent  Resultat = new Intent(SearchItineraryActivity.this, ViewSearchItineraryResultsListActivity.class);
+                    SearchRequestModel objet = new SearchRequestModel(Depart, Destination, Date);
+                    Intent Resultat = new Intent(SearchItineraryActivity.this, ViewSearchItineraryResultsListActivity.class);
 
-                    Resultat.putExtra( EXTRA_OBJET,objet);
+                    Resultat.putExtra(EXTRA_OBJET, objet);
                     startActivity(Resultat);
 
                 } else {
@@ -66,86 +79,49 @@ public class SearchItineraryActivity extends AppCompatActivity {
                     //display in short period of time
                     Toast.makeText(SearchItineraryActivity.this, getString(R.string.toast), Toast.LENGTH_SHORT).show();
                 }
+
             }
-
-            ;
-
 
         });
 
+        // Test du calendar,
+
+        myCalendar = Calendar.getInstance();
+        final DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
+
+            @RequiresApi(api = Build.VERSION_CODES.N)
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear,
+                                  int dayOfMonth) {
+                // TODO Auto-generated method stub
+                myCalendar.set(Calendar.YEAR, year);
+                myCalendar.set(Calendar.MONTH, monthOfYear);
+                myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                updateLabel();
+            }
+        };
+
+        editText10.setOnClickListener(new View.OnClickListener() {
 
 
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                new DatePickerDialog(SearchItineraryActivity.this, date, myCalendar
+                        .get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
+                        myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
-    /**
-     * ATTENTION: This was auto-generated to implement the App Indexing API.
-     * See https://g.co/AppIndexing/AndroidStudio for more information.
-     */
-    public Action getIndexApiAction() {
-        Thing object = new Thing.Builder()
-                .setName("SearchItinerary Page") // TODO: Define a title for the content shown.
-                // TODO: Make sure this auto-generated URL is correct.
-                .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
-                .build();
-        return new Action.Builder(Action.TYPE_VIEW)
-                .setObject(object)
-                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
-                .build();
-    }
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    private void updateLabel() {
+        String myFormat = "MM/dd/yy"; //In which you need put here
+        SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
 
-    @Override
-    public void onStart() {
-        super.onStart();
-
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        client.connect();
-        AppIndex.AppIndexApi.start(client, getIndexApiAction());
-    }
-
-    @Override
-    public void onStop() {
-        super.onStop();
-
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        AppIndex.AppIndexApi.end(client, getIndexApiAction());
-        client.disconnect();
+        editText10.setText(sdf.format(myCalendar.getTime()));
     }
 }
-
 
